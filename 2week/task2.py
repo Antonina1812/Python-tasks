566f0040646198eb6af20fc840149b32f618b32d
Antonina <146942940+Antonina1812@users.noreply.github.com>
2024-09-30 09:36:08 +0300

    Add files via upload

---
+++ 2week/task2.py
@@ -0,0 +1,28 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/decode-string/description/
+"""
+
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curr_num = 0
+        curr_str = ""
+
+        for char in s:
+            if char.isdigit():
+                curr_num = curr_num * 10 + int(char)
+            elif char == "[":
+                stack.append(curr_str)
+                stack.append(curr_num)
+                curr_str = ""
+                curr_num = 0
+            elif char == "]":
+                num = stack.pop()
+                prev_str = stack.pop()
+                curr_str = prev_str + num * curr_str
+            else:
+                curr_str += char
+
+        return curr_str
